82202d3ff6cab4196f2de59fd754086575e04446
shivajighasti/04 <shivajighasti97@gmail.com>
2022-08-01 23:29:40 +0530

    Added: Refactore the code to throw custom exceptions in case Invalid …

---
+++ main/java/com/bridgelabz/user/UserRegistrationException.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.user;
+
+public class UserRegistrationException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    public UserRegistrationException(String message) {
+        super(message);
+    }
+}
